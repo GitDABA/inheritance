bad491c3cdd12411555dcc83c2861330b9a3f796
David Bakke <david@daba.no>
2025-03-01 13:49:46 +0100

    Fix Netlify deployment configuration for Next.js SSR

---
+++ netlify/functions/nextjs.js
@@ -0,0 +1,5 @@
+// This file is needed for Netlify Next.js SSR functionality
+// The @netlify/plugin-nextjs package will use this handler
+// DO NOT MODIFY THIS FILE
+
+export { handler } from '@netlify/plugin-nextjs'
